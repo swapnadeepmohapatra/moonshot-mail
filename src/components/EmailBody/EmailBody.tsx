d5942ec93b86f3142dbdd03da5d47bd1e8603d89
Swapnadeep Mohapatra <41564532+swapnadeepmohapatra@users.noreply.github.com>
2024-10-15 20:23:58 +0530

    [Added] styles to mail body

--- src/components/EmailBody/EmailBody.tsx
+++ src/components/EmailBody/EmailBody.tsx
@@ -1,27 +1,38 @@
 import React from "react";
 import { useEmailContext } from "../../contexts/EmailContext";
+import styles from "./styles.module.css";
+import { formatDate } from "../../utils/formatDate";
 
 const EmailBody: React.FC = () => {
   const { selectedEmail } = useEmailContext();
 
   if (!selectedEmail) {
-    return (
-      <article>
-        <h2>No email selected</h2>
-      </article>
-    );
+    return <></>;
   }
 
   return (
-    <article>
-      <h2>{selectedEmail?.from?.name?.charAt(0)}</h2>
-      <h2>{selectedEmail?.subject}</h2>
-      <p>{new Date(selectedEmail?.date).toLocaleString()}</p>
-      <p
-        dangerouslySetInnerHTML={{
-          __html: selectedEmail?.body?.body || "",
-        }}
-      ></p>
+    <article className={styles.container}>
+      <section className={styles.header}>
+        <div className={styles.avatar}>
+          <h2>{selectedEmail?.from?.name?.charAt(0).toUpperCase()}</h2>
+        </div>
+        <div className={styles.mailSubject}>
+          <h2>{selectedEmail?.subject}</h2>
+          <p>{formatDate(selectedEmail?.date)}</p>
+        </div>
+        <button className={styles.favoriteButton}>
+          {selectedEmail?.isFavorite
+            ? "Remove from favorites"
+            : "Mark as favorite"}
+        </button>
+      </section>
+      <section className={styles.mailBody}>
+        <p
+          dangerouslySetInnerHTML={{
+            __html: selectedEmail?.body?.body || "",
+          }}
+        ></p>
+      </section>
     </article>
   );
 };
